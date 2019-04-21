0c7658d5a7e8e1a836539f244d523792bc8218be
Theo Weller <weller.theodor@gmail.com>
2019-04-21 23:45:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
-  if distance < 42
+  if (distance < 42) {
     return 42 % distance;
 } else {
     return distance  % 42;
 }
-};
+}
 // let answer;
 // distance > 42 ? answer = distance  % 42 : answer = 42 % distance;
