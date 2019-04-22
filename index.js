bc3d569b2751c243452a64efde5f0fb3cf7289e5
Theo Weller <weller.theodor@gmail.com>
2019-04-22 00:21:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,16 +23,16 @@ function calculatesFarePrice(start, destination) {
   const distanceTravelled = distanceFromHqInFeet(start, destination)
 
   switch (distanceTravelled) {
-    case: distanceTravelled < 400;
+    case distanceTravelled < 400:
       return 0;
       break;
-    case: distanceTravelled > 400 && distanceTravelled < 2000;
+    case distanceTravelled > 400 && distanceTravelled < 2000:
       return (distanceTravelled - 400) * 0.2;
       break;
-    case: distanceTravelled > 2000 && distanceTravelled < 2500;
+    case distanceTravelled > 2000 && distanceTravelled < 2500:
       return 25;
       break;
-    case: distanceTravelled > 2500;
+    case distanceTravelled > 2500:
       return 'cannot travel that far';
       break;
 
