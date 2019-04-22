4d2eef15ad66d18d01c806f528b0d94781b2a98c
Theo Weller <weller.theodor@gmail.com>
2019-04-22 00:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,22 @@ function distanceTravelledInFeet(arg1, arg2) {
     return arg1 % arg2 * 264;
   }
 };
+
+function calculatesFarePrice(start, destination) {
+  const distance_travelled = distanceFromHqInFeet(start, destination)
+
+  switch (distance_travelled) {
+    case: distance_travelled < 400;
+      return 0;
+      break;
+    case: distance_travelled > 400 && distance_travelled < 2000;
+      return (distance_travelled - 400) * 0.2;
+      break;
+    case: distance_travelled > 2000 && distance_travelled < 2500;
+      return 25;
+      break;
+    case: distance_travelled > 2500;
+      return 'cannot travel that far';
+
+  }
+};
